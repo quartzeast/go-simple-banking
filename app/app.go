5cd60974ab9749c764c9afaad281323bd8f83c65
shiyingdong <quartzeast@gmail.com>
2024-04-01 16:57:03 +0800

    gorialla/mux

--- app/app.go
+++ app/app.go
@@ -3,13 +3,17 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 	// define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/quartzeast/go-simple-banking
 
 go 1.22.0
+
+require github.com/gorilla/mux v1.8.1
